033fdc9f214ac0832367a51eca67ddba62363d96
caileihh <73216148+caileihh@users.noreply.github.com>
2021-12-10 00:42:20 +0800

    Add files via upload

--- prai.c
+++ prai.c
@@ -5,13 +5,12 @@ int main(int argc, char * argv[]) {
     char command_esedbexport[10000];
 
 //    char *command_esedbexport;
-    strcpy(command_esedbexport,"cd /home/eda210506 && java -jar untitled.jar ");
+    strcpy(command_esedbexport,"cd /home/eda210506/project/code && java -jar untitled.jar ");
     strcat(command_esedbexport,argv[1]);
     strcat(command_esedbexport," ");
     strcat(command_esedbexport,argv[2]);
     strcat(command_esedbexport," ");
-    strcat(command_esedbexport,"/home/eda210506/ModuleResult.txt");
+    strcat(command_esedbexport,"/home/eda210506/project/verify_results/result_.txt ");
+    strcat(command_esedbexport,"/home/eda210506/project/verify_results/ModuleResult_.txt");
     system(command_esedbexport);
-//    printf("%s",command_esedbexport);
-//    for(int i=1;i<argc;i++) printf("%s\n",argv[i]);
 }
